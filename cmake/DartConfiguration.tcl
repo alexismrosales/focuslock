43c036c8b7a52e182711dd8a19334eae06901dee
alexismrosales <aleisjorge91@gmail.com>
2025-01-09 21:53:02 -0600

    chore: update project name config

--- cmake/DartConfiguration.tcl
+++ cmake/DartConfiguration.tcl
@@ -4,8 +4,8 @@
 
 
 # Configuration directories and files
-SourceDirectory: /home/rarksz/Programs/cpp-project-template
-BuildDirectory: /home/rarksz/Programs/cpp-project-template/cmake
+SourceDirectory: /home/rarksz/Programs/focuslock
+BuildDirectory: /home/rarksz/Programs/focuslock/cmake
 
 # Where to place the cost data store
 CostDataFile: 
@@ -27,7 +27,7 @@ SubmitInactivityTimeout:
 NightlyStartTime: 00:00:00 EDT
 
 # Commands for the build/test/submit cycle
-ConfigureCommand: "/usr/bin/cmake" "/home/rarksz/Programs/cpp-project-template"
+ConfigureCommand: "/usr/bin/cmake" "/home/rarksz/Programs/focuslock"
 MakeCommand: /usr/bin/cmake --build . --config "${CTEST_CONFIGURATION_TYPE}"
 DefaultCTestConfigurationType: Release
 
