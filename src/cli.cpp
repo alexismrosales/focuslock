40bac20e0265bcff6cc8c90116409b164407f8ea
alexismrosales <aleisjorge91@gmail.com>
2025-01-15 02:28:38 -0600

    feat: new commands added

--- src/cli.cpp
+++ src/cli.cpp
@@ -33,12 +33,70 @@ void newSession(const std::map<std::string, std::string> &args) {
   }
 }
 
+void setSession(const std::map<std::string, std::string> &args) {}
+void status(const std::map<std::string, std::string> &args) {}
+void block(const std::map<std::string, std::string> &args) {}
+void start(const std::map<std::string, std::string> &args) {}
+void stop(const std::map<std::string, std::string> &args) {}
+void listSessions(const std::map<std::string, std::string> &args) {}
+void enable(const std::map<std::string, std::string> &args) {
+  for (const auto &[cmd, vals] : args) {
+    std::cout << "command: " << cmd << ", values: " << vals << std::endl;
+  }
+}
+void disable(const std::map<std::string, std::string> &args) {}
+
 void createCommands(CommandRegistry &registry, CLI::App &app) {
+  registry.addCommand(
+      Command(app, "new-session",
+              "create a new session for focuslock. You can customize the "
+              "session later using other commands.",
+              newSession, {{"--name", "name of the session to create"}}));
+
+  registry.addCommand(
+      Command(app, "set-session",
+              "set the specified session as active. \n\t The active session "
+              "will be used for future commands until changed.",
+              setSession, {{"--name", "name of existing session"}}));
+
+  registry.addCommand(
+      Command(app, "status",
+              "display the current status of focuslock, including the active "
+              "session and the status of enabled modules.",
+              status));
+
+  registry.addCommand(
+      Command(app, "block",
+              "add, remove, or list blocked URLs and directories. \n\t Use "
+              "subcommands to modify the block list.",
+              block,
+              {{"--add", "add a URL or directory to the block list"},
+               {"--remove", "remove a URL or directory from the block list"},
+               {"--list", "show all blocked items"}}));
+
+  registry.addCommand(
+      Command(app, "list-sessions",
+              "show a list of all sessions currently available in focuslock. "
+              "This includes their names and status",
+              listSessions));
+
   registry.addCommand(Command(
-      app, "new-session",
-      "create a new session for focuslock. You can customize the session "
-      "later using other commands.",
-      newSession, {{"--name", "name of the session to create"}}));
+      app, "start",
+      "start focuslock and activate the configured modules. If a session name "
+      "is provided, switches to that session before starting",
+      start,
+      {{"--name", " name of an existing session to switch to before start"}}));
+  registry.addCommand(Command(app, "stop", "reset any ongoing sessions", stop));
+  registry.addCommand(Command(app, "enable",
+                              "activate one or more modules for focuslock. "
+                              "Modules must be specified by name",
+                              enable, {{"--modules", "modules to enable"}},
+                              true));
+  registry.addCommand(Command(app, "disable",
+                              "deactivate one or more modules for focuslock. "
+                              "Modules must be specified by name",
+                              disable, {{"--modules", "modules to enable"}},
+                              true));
 }
 
 // run_cli read arguments of program execution and handle them
