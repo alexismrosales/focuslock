009bc361631081337d92dd8e94d12b3bc0d97958
alexismrosales <aleisjorge91@gmail.com>
2025-01-24 02:13:51 -0600

    chore: added parsing file into a yaml into load sessions func

--- src/sessions.cpp
+++ src/sessions.cpp
@@ -1,13 +1,29 @@
 #include "sessions/sessions.h"
 #include "config/config.hpp"
+#include <ryml/ryml.hpp>
 #include <vector>
 
 namespace sessions {
 std::vector<Session> loadSessions() {
+  std::string yamlSessions = config::loadConfig();
+  // in case there are no sessions
+  if (yamlSessions == "") {
+    return {};
+  }
+  ryml::Tree t = ryml::parse_in_place(ryml::to_substr(yamlSessions));
+  ryml::ConstNodeRef root = t.rootref(); // a const node reference
   std::vector<Session> sessionsList;
-  // log
-  std::cout << "content loaded:" << std::endl
-            << config::loadConfig() << std::endl;
+
+  // for every session in yaml
+  for (ryml::ConstNodeRef node : root.children()) {
+    // TODO: convert every child in tree into a Session struct object,
+    // transforming every node into the values of the struct Session
+    if (node.has_child("session")) {
+      std::cout << "Session: " << node["session"].val() << std::endl;
+      std::cout << "Pomodoro Work Time: "
+                << node["settings"]["pomodoro"]["work"].val() << std::endl;
+    }
+  }
   return sessionsList;
 }
 } // namespace sessions
