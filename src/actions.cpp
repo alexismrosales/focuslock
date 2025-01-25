784e05a6b1bdf0de04ea8740265fc528a232325a
alexismrosales <aleisjorge91@gmail.com>
2025-01-25 01:30:50 -0600

    feat: new-session command implementation done

--- src/actions.cpp
+++ src/actions.cpp
@@ -1,13 +1,24 @@
 #include "actions/actions.h"
-#include "serialization/serialization.hpp"
 #include "sessions/sessions.h"
 #include <iostream>
+#include <vector>
 
 namespace actions {
 void NewSession(std::string name) {
   // creating new session
   sessions::Session s = {name, sessions::Settings{}};
-  serialization::serializeSession(s);
-  sessions::loadSessions();
+  std::vector<sessions::Session> sessions = sessions::loadSessions();
+  // verify session name duplication
+  for (auto &session : sessions) {
+    if (session.name == name) {
+      std::cerr << "Error creating session: session name already exist"
+                << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+  }
+  // add new session
+  sessions.push_back(s);
+
+  sessions::saveSessions(sessions);
 }
 } // namespace actions
