179e10e9e5534fa736cd4444920275b19972bcc2
Alexis M. Rosales <alexismrosales@Alexiss-MacBook-Pro.local>
2026-06-07 21:42:03 -0600

    feat: initialize setup for core version

---
+++ include/core/engine.h
@@ -0,0 +1,4 @@
+#pragma once
+namespace core {
+int startDaemon(int argc, char *argv[]);
+}
---
+++ src/core/engine.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+namespace core {
+int startDaemon(int argc, char *argv[]) {
+  std::cout << "Hello world from StartDaemon!" << std::endl;
+  return 0;
+}
+} // namespace core
